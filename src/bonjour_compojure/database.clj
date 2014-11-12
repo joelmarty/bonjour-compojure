0517991a11b1deb0449a1177ed03e40aee0fccd0
Joël Marty <joel@lolnet.org>
2014-11-12 15:19:29 +0100

    dbapi/delete + unit test

--- src/bonjour_compojure/database.clj
+++ src/bonjour_compojure/database.clj
@@ -1,9 +1,10 @@
 (ns bonjour_compojure.database
   (:require [monger.core :as mg]
             [monger.collection :as mc]
-            [monger.conversion :refer [from-db-object]]
+            [monger.conversion :refer [from-db-object to-object-id]]
             [nomad :refer [defconfig]]
-            [clojure.java.io :as io]))
+            [clojure.java.io :as io]
+            [monger.query :as mq]))
 
 (defconfig app-config (io/resource "app.edn"))
 
@@ -11,18 +12,24 @@
 (def bonjourdb (atom nil))
 
 (defn init
-  "initializes the connection to the database and returns the atom representing the db object.
+  "Initializes the connection to the database and returns the atom representing the db object.
+  Is side-effecting by creating the database infrastructure if it doesn't exist yet.
   Should be called at app startup."
   []
   (let [config (app-config)
         host (:mongo-host config)
         port (:mongo-port config)
-        database (:database config)]
+        database (:database config)
+        collection (:bonjour config)]
     (if (and (nil? host) (nil? port))
       (reset! conn (mg/connect))
       (reset! conn (mg/connect {:host host :port port})))
     (if (not (nil? @conn))
-      (reset! bonjourdb (mg/get-db @conn database))
+      (do
+        (reset! bonjourdb (mg/get-db @conn database))
+        (if (not (mc/exists? @bonjourdb collection))
+          (mc/create @bonjourdb collection {:capped false}))
+        (mc/ensure-index @bonjourdb collection {"date" 1}))
       (throw (RuntimeException.
              (str "The connection to" host ":" port "/" database "failed to open"))))))
 
@@ -36,22 +43,37 @@
 (defn find-by-date
   "finds a bonjour by date"
   [date]
-  (let [config (app-config)
-        collection (:bonjour config)]
+  (let [collection (:bonjour (app-config))]
     (from-db-object (mc/find-one @bonjourdb collection {:date date}) true)))
 
+(defn add
+  "adds a new bonjour to the db"
+  [bonjour]
+  (let [collection (:bonjour (app-config))]
+    (from-db-object (mc/insert-and-return @bonjourdb collection bonjour) true)))
 
-;; (defn collection-exists?
-;;   "tests the existence of the named collection"
-;;   [collection]
-;;   (mc/exists? @bonjourdb collection))
+(defn list
+  "lists the bonjour items in the db. Takes optional page and length arguments"
+  ([]
+    (into [] (mc/find-maps @bonjourdb (:bonjour (app-config)))))
+  ([page]
+    (list page 10))
+  ([page length]
+    (mq/with-collection @bonjourdb (:bonjour (app-config))
+      (mq/find {})
+      (mq/sort {:date -1})
+      (mq/paginate :page page :per-page length))))
 
-;; (defn create-collection
-;;   "Creates the bonjour collection. Takes the bonjour app subject as argument."
-;;   [subject]
-;;   (if (not (collection-exists? subject))
-;;     (mc/create @bonjourdb subject)))
-
+(defn delete
+  "Deletes a bonjour by id. Returns the deleted element"
+  [id]
+  (let [collection (:bonjour (app-config))
+        deleted-bonjour (mc/find-one-as-map @bonjourdb collection {:_id (to-object-id id)})]
+    (if (not (nil? deleted-bonjour))
+     (do
+       (mc/remove-by-id @bonjourdb collection id)
+       deleted-bonjour)
+     nil)))
 
 
 
--- test/bonjour_compojure/test/database.clj
+++ test/bonjour_compojure/test/database.clj
@@ -8,11 +8,10 @@
 
 (defconfig app-config (io/resource "app.edn"))
 
-(defn create-db [connection db-name collection-name]
+(defn reset-db [connection db-name collection-name]
   (let [db (mg/get-db connection db-name)]
     (if (mc/exists? db collection-name)
       (mc/drop db collection-name))
-    (mc/create db collection-name {:capped false})
     (mg/disconnect connection)))
 
 (defn delete-db [connection db-name]
@@ -26,13 +25,24 @@
         test-db (:database config)
         test-collection (:bonjour config)]
     (let [conn (mg/connect {:host host :port port})]
-      (create-db conn test-db test-collection))
+      (reset-db conn test-db test-collection))
     (dbapi/init)
     (test-fn) ;; call to test is explicit
     (let [conn (mg/connect {:host host :port port})]
       (delete-db conn test-db))))
 
+(defn clean-collection [test-fn]
+  (let [config (app-config)
+        test-db (:database config)
+        test-collection (:bonjour config)]
+    (test-fn)
+    (mc/remove @dbapi/bonjourdb test-collection)))
+
+(defn test-data []
+  (map #(array-map :date (format "2014-09-%02d" %) :image (format "2014/09/%02d.jpg" %)) (range 18 28)))
+
 (use-fixtures :once setup)
+(use-fixtures :each clean-collection)
 
 (deftest test-db
 
@@ -46,4 +56,36 @@
       (let [created-bonjour (dbapi/find-by-date date)]
         (is (not (nil? created-bonjour)))
         (is (= (:date created-bonjour) "2014-09-16")))))
-  )
+
+  (testing "adds a bonjour"
+    (let [new_bjr {:date "2014-09-17" :image "2014/09/17.jpg"}
+          created_bjr (dbapi/add new_bjr)]
+      (is (not (nil? created_bjr)))
+      (is (contains? created_bjr :_id))
+      (is (contains? created_bjr :date))
+      (is (contains? created_bjr :image))))
+
+  (testing "gets the list of all bonjour"
+    (let [test-collection (:bonjour (app-config))
+          documents (into [] (test-data))]
+      (mc/insert-batch @dbapi/bonjourdb test-collection documents)
+      (let [docs-in-db (dbapi/list)]
+        (is (<= 10 (count docs-in-db))))))
+
+  (testing "gets the list of 5 items in first page of bonjours"
+    (let [test-collection (:bonjour (app-config))
+          documents (into [] (test-data))]
+      (mc/insert-batch @dbapi/bonjourdb test-collection documents)
+      (let [docs-in-db (dbapi/list 1 5)]
+        (is (= 5 (count docs-in-db))))))
+
+  (testing "deletes a bonjour by id"
+    (let [test-collection (:bonjour (app-config))
+          documents (into [] (test-data))
+          last_inserted (last documents)]
+      (mc/insert-batch @dbapi/bonjourdb test-collection documents)
+      (let [to_delete (dbapi/list 1 1)
+            deleted (dbapi/delete (:_id (first to_delete)))]
+        (is (= (:date deleted) (:date last_inserted)))
+        (is (= (:image deleted) (:image last_inserted))))))
+)
